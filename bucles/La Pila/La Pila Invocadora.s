ecf917e02f340dce31a9f4092d89c80e88774975
TheMrRafus <themrrafus@gmail.com>
2018-01-30 00:35:50 +0100

    Corregido registros

--- bucles/La Pila/La Pila Invocadora.s
+++ bucles/La Pila/La Pila Invocadora.s
@@ -8,16 +8,16 @@ maluma: .space 101
 .text
 .globl __start
 __start:
-        la $6,maluma
+        la $4,maluma
         li $5,100
         get_cadena
         li $9,0
 bucleone: beq $9,5,fin
           lb $10,vocales($9)
           subu $30,$39,4    #Restamos un word al stack pointer
-          sw $9,0($30)
+          sw $9,0($30) #guardamos en la pila
           jal rutina
-          
+
 
 
 
