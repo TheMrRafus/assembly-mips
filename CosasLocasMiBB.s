0ed1f080564e3e3cf44c2a0cdf63b32f8f8c7a77
Antonio Gines <antonio57gines57@gmail.com>
2018-01-31 19:28:04 +0100

    DesPaCito

--- CosasLocasMiBB.s
+++ CosasLocasMiBB.s
@@ -1,11 +1,12 @@
-    .data
-vocales:      .asciiz     "aeiou"
-maluma: .asciiz     "Mete a BadBunny: "
-ozuna:    .asciiz     "El número de vocales es: "
-negritoojosclaros:     .asciiz     "\n"
-badbunny:        .space      100
-pollas: .space 5
-
+#################################################################################
+#    .data
+#   vocales:      .asciiz     "aeiou"
+#   maluma: .asciiz     "Mete a BadBunny: "
+#   ozuna:    .asciiz     "El número de vocales es: "
+#   negritoojosclaros:     .asciiz     "\n"
+#   badbunny:        .space      100
+#   pollas: .space 5
+#
 ###################################Esto realmente sobraba########################
 #.text
 #    .globl  main
@@ -70,3 +71,165 @@ pollas: .space 5
 ###################################Esto realmente sobraba########################
 
 
+.data
+vocales: .asciiz "aeiou"
+result: .asciiz " se repite "
+resulta2: .asciiz " veces"
+bitParidad: .asciiz "\n-------------------------\n"
+frase: .space 101
+pregunta: .asciiz "Introduzca una frase de 30 caract. max\n\n"
+salto: .asciiz "\n"
+pollas: .space 5
+
+
+#Atemporal, largo e inutil
+#AAA: .space 1
+#EEE: .space 1
+#III: .space 1
+#OOO: .space 1
+#UUU: .space 1
+
+
+.text
+.globl __start
+__start:
+	li $t0, 0    ##Variable que usamos como contador 
+	li $t5, 5   ##Numero maximo de iteraciones
+	
+	##------Preguntamos la frase y la leemos--------
+	
+	li $2, 4
+	la $4, pregunta
+	syscall
+	li $2, 8
+	la $4, frase
+	li $5, 30
+	syscall
+	
+	##---------------------------------------------------------------------------------------##
+	                                                                                         ##
+	bucle:                                                                                   ##
+		beq $t0, 5, tracaFinal ##Si el contador es 5 finalizamos                         ##
+		lb $s0, vocales($t0)   ##Guardamos vocalXvocal en $s0 (static)                   ##     
+		                                                                                 ##
+		                                                                                 ##
+		##-----------Convenio de pila-----------------##                                 ##
+			subu $sp, $sp, $t9                                                       ##
+			##--Restamos 4 posiciones y guardamos el RA($31) actual en la pila       ##
+			move $ra, $sp                                                            ##
+		##--------------------------------------------##                                 ##
+												 ##		
+		 										 ##
+		li $t3,0 ##Usado como contador del bucle repeat-until                            ##
+		li $s1, 0 ##Usado para contar el numero de repeticiones                          ##
+		jal comparados  ##Comenzamos el bucle para comparar cada vocal                   ##
+		#####INUSADO########jal guardarResultados                                                  ##
+											         ##
+												 ##		
+		##-----------Convenio de pila-----------------##                                 ##
+			move $sp, $ra                                                            ##
+			##El RA anterior lo sacamos de la pila y lo                              ##
+			##----------------devolvemos al RA($31)                                  ##
+			addu $sp,$sp, $t9                                                        ##
+		##--------------------------------------------##                                 ##
+											         ##		
+												 ##
+		add $t0,$t0,1                                                                    ##
+		j bucle                                                                          ##
+												 ##
+	##---------------------------------------------------------------------------------------##	
+	
+    
+    ###############Bastante inutil, complejo y bastardo###########
+	#guardarResultados:
+	#	beq $t0, 0, guardoA
+	#	beq $t0, 1, guardoE
+	#	beq $t0, 2, guardoI
+	#	beq $t0, 3, guardoO
+	#	beq $t0, 4, guardoU
+	#	
+	#guardoA: 
+	#	sb $s1, AAA
+	#	jr $ra	
+	#guardoE: 
+	#	sb $s1, EEE
+	#	jr $ra	
+	#guardoI: 
+	#	sb $s1, III
+	#	jr $ra	
+	#guardoO: 
+	#	sb $s1, OOO
+	#	jr $ra	
+	#guardoU: 
+	#	sb $s1, UUU
+	#	jr $ra	
+	##############################################################	
+			
+	##----------------------------------------------------------##
+	comparados:                                                 ##
+		lb $t4, frase($t3)                                  ##
+		add $t3, $t3, 1	                                    ##
+		                                                    ##
+		beq $s0, $t4, addOnePosition                        ##
+		bne $s0, $t4, noIguala                              ##
+		addOnePosition:	add $s1, $s1,1                      ##
+		noIguala:  blt $t3, 100, comparados                 ##
+		blt $t3, 31, comparados                            ##
+		                                                    ##
+		                                                    ##
+		jr $ra                                              ##
+	##----------------------------------------------------------##	
+		
+	tracaFinal: 
+		li $2, 4
+        li $t0, 0
+        
+		la $4, bitParidad
+		                        syscall
+		lb $4, vocales($t0)
+		                        syscall 
+		la $4, result
+		                        syscall
+		lb $4, pollas($t0)
+		                        syscall
+		la $4, resulta2
+		                        syscall
+		lb $4, salto
+		                        syscall
+		addi $t0, $t0,1
+		beq $t0, 5, mae
+		j tracaFinal
+	
+	imprimirResultados:
+		beq $t0, 0, imprimoa
+		beq $t0, 1, imprimoe
+		beq $t0, 2, imprimoi
+		beq $t0, 3, imprimoo
+		beq $t0, 4, imprimou
+		
+	imprimoa:
+		la $4, AAA
+		syscall
+		jr $ra
+	imprimoe:
+		la $4, EEE
+		syscall
+		jr $ra
+	imprimoi:
+		la $4, III
+		syscall
+		jr $ra
+	imprimoo:
+		la $4, OOO
+		syscall
+		jr $ra
+	imprimou:
+		la $4, UUU
+		syscall
+		jr $ra
+	
+	
+	
+	
+	mae: .end
+	
